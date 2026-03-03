e22cbe85559e356388332187aab2f09d7e16a005
mootezammar <mootezammar4@gmail.com>
2026-03-03 03:39:37 +0100

    fix: webhook before express.json

--- server/server.js
+++ server/server.js
@@ -5,25 +5,22 @@ import { clerkMiddleware } from '@clerk/express'
 import connectDB from "./config/mongodb.js"
 import clerkWebhooks from "./controllers/clerkWebHooks.js"
 
-await connectDB() // connection to data base
+await connectDB()
 
-const app=express() //initialize express app
-app.use(cors()) // enables corss origin to shared with frontend
-
-
-//Middleware setup
-app.use(express.json()) // enable json requeste for body
+const app = express()
+app.use(cors())
 app.use(clerkMiddleware())
 
-// api to listen to clerk webhooks
-app.use("/api/clerk", express.raw({ type: "application/json" }), clerkWebhooks)
+// ✅ AVANT express.json()
+app.post("/api/clerk", express.raw({ type: "application/json" }), clerkWebhooks)
 
-// Route check
+// ✅ APRÈS le webhook
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.json({ success: true, message: "API works successfully" })
 })
 
-const port=process.env.PORT || 4000  // definir server port 
+const port = process.env.PORT || 4000
 
-//start server
-app.listen(port , ()=>console.log(`server is running at http://localhost:${port}`))
+app.listen(port, () => console.log(`server is running at http://localhost:${port}`))
